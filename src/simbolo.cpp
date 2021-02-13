593fc53bf92f999633c259a0b1a8c88820b35894
AdrianGarcia04 <adrian.garcia04@ciencias.unam.mx>
2021-02-12 18:58:40 -0600

    Aceptando comentarios

--- src/simbolo.cpp
+++
@@ -1,6 +0,0 @@
-#include "simbolo.h"
-
-simbolo *crea_simbolo(string id, int dir, int type, int var, vector<int> args) {
-  //implementa
-  return NULL;
-}
--- src/tipo.cpp
+++
@@ -1,5 +0,0 @@
-#include "tipo.h"
-
-simbolo *crea_entrada_tipo(int id, char *tipo, int tam, int elem, int tipoBase) {
-  //implementa
-}
