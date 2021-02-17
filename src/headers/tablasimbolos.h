7a721e635d006e5167826de1fdc7e27e5dee2ffd
AdrianGarcia04 <adrian.garcia04@ciencias.unam.mx>
2021-02-16 19:47:42 -0600

    Tablasimbolos

--- src/headers/tablasimbolos.h
+++ src/headers/tablasimbolos.h
@@ -1,30 +1,69 @@
 #pragma once
-
-#include <stack>
-#include <vector>
 #include <string>
-#include "simbolo.h"
-
+#include <list>
 using namespace std;
 
-typedef struct tabla_simbolos{
-    vector<simbolo> tabla;
-public:
-    bool buscar(string id);
-    bool insertar(simbolo s);
-    int getTipo(string id);
-    void imprimir();
-} tabla_simbolos;
+struct simbolo {
+  string id;
+  int dir;
+  int tipo;
+  int var;
+  list<int> args;
 
-typedef struct pila_simbolos{
-    stack<tabla_simbolos*> pila;
-public:
-    void push(tabla_simbolos* tabla);
-    void pop();
-    tabla_simbolos* top();
-    void insertar_tabla();
-} pila_simbolos;
+  simbolo(){}
+  simbolo(string id, int dir, int tipo, int var, list<int> args) : id(id), dir(dir), tipo(tipo), var(var), args(args){}
 
-tabla_simbolos* crear_tabla_simbolos();
+  string str() {
+    return id
+        + "\t" + std::to_string(dir)
+        + "\t" + std::to_string(tipo)
+        + "\t" + std::to_string(var);
+  }
 
-pila_simbolos* crear_pila_simbolos();
+  bool operator==(const struct simbolo& a) const {
+    return a.id == this->id;
+  }
+};
+
+struct tablasimbolos {
+  list<simbolo> tabla;
+
+  tablasimbolos(){}
+  tablasimbolos(list<simbolo> tabla) : tabla(tabla){}
+
+  string str() {
+    string str = "";
+    for (simbolo simbolo : tabla)
+        str = str + simbolo.str() + "\n";
+    return str;
+  }
+
+  void agregar(simbolo simbolo) {
+    tabla.push_back(simbolo);
+  }
+
+  void eliminar(simbolo simbolo) {
+    tabla.remove(simbolo);
+  }
+
+  bool buscar(string id) {
+    for (simbolo simbolo : tabla)
+        if (simbolo.id == id)
+            return true;
+    return false;
+  }
+
+  int get_tipo(string id) {
+    for (simbolo simbolo : tabla)
+        if (simbolo.id == id)
+            return simbolo.tipo;
+    return -1;
+  }
+
+  int get_var(string id) {
+    for (simbolo simbolo : tabla)
+        if (simbolo.id == id)
+            return simbolo.var;
+    return -1;
+  }
+};
